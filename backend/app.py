b2f247d50a44702f0e756b3cb2bd78916f8dddf9
zee <zerex68@gmail.com>
2024-12-01 12:20:48 +0530

    changes in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello Flask!"
+    return jsonify({"message": "Welcome to the Automator API!"})
 
 
 if __name__ == "__main__":
