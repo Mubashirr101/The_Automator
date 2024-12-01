b21c86efef6c1b52743cc3ce833ec609d12b6045
zee <zerex68@gmail.com>
2024-12-01 16:42:36 +0530

    wrote code for the input of a csv file and the desc() of the dataset as the output, new html file for the output added

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,72 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+# Ensure the uploads folder exists
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["ALLOWED_EXTENSIONS"] = {"csv"}
+
+
+def allowed_file(filename):
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
+    )
 
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Welcome to the Automator API!"})
+    return "Welcome to The Automator"
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        # Check if the request contains a file
+        if "file" not in request.files:
+            return "No file part", 400
+        file = request.files["file"]
+
+        # If the user did not select a file
+        if file.filename == "":
+            return "No selected file", 400
+
+        # Check if the file has the correct extension
+        if file and allowed_file(file.filename):
+            filename = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+            file.save(filename)
+            return (
+                f"""printmessage: file uploaded successfully: {filename}
+                  File Analysis : {process_File(filename)}""",
+                200,
+            )
+        else:
+            return "Invalid file type. Only CSV files are allowed", 400
+
+    # For GET request, display the file upload form
+    return """
+    <!doctype html>
+    <title>Upload a CSV File</title>
+    <h1>Upload a CSV File</h1>
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit" value="Upload">
+    </form>
+    """
+
+
+# @app.route("/process", methods=["GET"])
+def process_File(filename):
+    file_path = os.path.join(filename)
+    df = pd.read_csv(file_path)
+
+    summary = df.describe().to_html(classes="table table-striped")
+    return render_template("analysis.html", table=summary)
 
 
 if __name__ == "__main__":
