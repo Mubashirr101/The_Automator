90a67262cdf427d36abf566d555c96e9d0be73d4
Mubashir <zerex68@gmail.com>
2025-01-11 16:44:46 +0530

    wrote more codes for data viz mod

--- backend/modules/DataViz.py
+++ backend/modules/DataViz.py
@@ -1,84 +1,189 @@
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
-import seaborn as sns
-import plotly as ply
-
-# import bokeh
-# import altair
-# import missingno
+# Types of data viz
+import matplotlib.pyplot as plt
 
 
 class vizualize:
-    def __init__(self, df):
-        self.data = df
-
-    def overview():
-        # histograms
-        # box/whisker plots
-        # bar charts
-        # Pie charts
+    def __init__(self):
         pass
 
-    def data_distribution():
+    # Overview #
+    def histo(self, col):
+        # histograms
+        plt.hist(col, bins=5, alpha=0.5, label=col.name)
+        plt.xlabel(col.name)
+        plt.ylabel("frequency")
+        plt.title(f"Histogram of {col.name}")
+        plt.legend()
+        plt.show()
+
+    def boxplt(self, col):
+        # box plot
+        plt.boxplot(col)
+        plt.xlabel(col.name)
+        plt.ylabel("values")
+        plt.title(f"Boxplot of {col.name}")
+        plt.show()
+
+    def barchrt(self, col):
+        # bar charts
+        col.value_counts().plot(kind="bar")
+        plt.xlabel(col.name)
+        plt.ylabel("frequency")
+        plt.title(f"Bar chart of {col.name}")
+        plt.show()
+
+    def piechrt(self, col):
+        # Pie charts
+        col.value_counts().plot(kind="pie")
+        plt.xlabel(col.name)
+        plt.ylabel("frequency")
+        plt.title(f"Pie chart of {col.name}")
+        plt.show()
+
+    # Data Distribution
+
+    def densityplots():
         # density plots
+        pass
+
+    def violinplots():
         # violin plots
+        pass
+
+    def ridgeplots():
         # ridge plots
         pass
 
-    def missing_data_viz():
+    # missing data vizualizations
+    def heatmaps():
         # heatmaps
+        pass
+
+    def missingness_matrix():
         # missingness matrix
+        pass
+
+    def barplots_na():
         # barplots for na counts
         pass
 
-    def relationship_viz():
+    # vizualization for relationships
+    def scatterplt():
         # scatter plots
+        pass
+
+    def bubblechrts():
         # bubble charts
+        pass
+
+    def pairplots():
         # pair plots
         pass
 
-    def categorical_data_rel_viz():
+    # catrgoricak data relationship vizualization
+    def barplts_cat():
         # barplots
+        pass
+
+    def cnt_plts():
         # count plts
+        pass
+
+    def stacked_brplts():
         # stacked bar charts
         pass
 
-    def multivariate_dist_viz():
+    # multivariate distribution vizualization
+    def heatmaps():
         # heatmaps
+        pass
+
+    def facetgrids():
         # facet grids
+        pass
+
+    def plots_3d():
         # 3d plots
         pass
 
-    def time_series_viz():
+    # time series vizualization
+    def line_chrts():
         # line charts
+        pass
+
+    def area_chrts():
         # area charts
+        pass
+
+    def heatmps():
         # heatmaps
+        pass
+
+    def lag_plots():
         # lag plots
         pass
 
-    def outlier_viz():
+    # outlier vizualization
+
+    def outlier_boxplots():
         # boxplots
+        pass
+
+    def outlier_violinplots():
+        # violin plots
+        pass
+
+    def outlier_scatterplots():
         # scatter plots
+        pass
+
+    def outlier_iqrplots():
         # z-score / IQR vizualization
         pass
 
-    def geo_data_viz():
+    # geographic data vizualization
+    def choropleth_mps():
         # choropleth maps
+        pass
+
+    def bubble_mps():
         # bubble maps
+        pass
+
+    def heatmaps_mps():
         # heatmaps on maps
         pass
 
-    def adv_viz():
+    # advanced data viz
+    def prll_coord_plts():
         # parallel coordinate plots
+        pass
+
+    def rad_chrts():
         # radial charts (spider/radar charts)
+        pass
+
+    def word_cnts():
         # word counts
+        pass
+
+    def sankey_diagrams():
         # sankey diagrams
         pass
 
-    def ml_viz():
+    # Machine Learning data viz
+    def feat_imp_plts():
         # feature imp plots
+        pass
+
+    def resid_plts():
         # residual plots
+        pass
+
+    def roc_curves():
         # ROC curve
+        pass
+
+    def conf_mat():
         # Conf Matrix
         pass
